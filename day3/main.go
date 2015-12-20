f3e0eb65d32800bba3e3fcb4273d93eecd5f4d14
Aaron Mills <aaron.mills@returnpath.com>
2015-12-20 15:24:42 -0700

    just ignore more

--- day3/main.go
+++ day3/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	log.SetLevel(log.DebugLevel)
+	log.SetLevel(log.ErrorLevel)
 
 	data, err := ioutil.ReadFile("input")
 	if err != nil {
@@ -21,22 +21,22 @@ func main() {
 	seen[point] = true
 
 	for _, d := range data {
-		var p [2]int
+		p := [2]int{}
 		d := string(d)
 
 		switch d {
 		case ">":
 			log.Debugf("Got %s, going east", d)
-			p = [2]int{point[0] + 1, point[1]}
+			p[0], p[1] = point[0]+1, point[1]
 		case "<":
 			log.Debugf("Got %s, going west", d)
-			p = [2]int{point[0] - 1, point[1]}
+			p[0], p[1] = point[0]-1, point[1]
 		case "^":
 			log.Debugf("Got %s, going north", d)
-			p = [2]int{point[0], point[0] + 1}
+			p[0], p[1] = point[0], point[1]+1
 		case "v":
 			log.Debugf("Got %s, going south", d)
-			p = [2]int{point[0], point[0] - 1}
+			p[0], p[1] = point[0], point[1]-1
 		}
 
 		if seen[p] == true {
