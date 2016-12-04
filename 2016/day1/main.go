4b6a485b63969bc50939263945888e8e54d4fdad
Aaron Mills <fluxrad@gmail.com>
2016-12-03 18:44:17 -0700

    finish day 1

--- 2016/day1/main.go
+++ 2016/day1/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
+	"os"
 	"strconv"
 	"strings"
 
@@ -12,11 +13,20 @@ import (
 
 type Location struct {
 	Orientation int
-	X           int
-	Y           int
+	*Coords
 }
 
-func (l *Location) Go(turn string, blocks int) {
+type Coords struct {
+	X, Y int
+}
+
+var visited map[Coords]bool
+
+func (c *Coords) String() string {
+	return fmt.Sprintf("[%d,%d]", c.X, c.Y)
+}
+
+func (l *Location) Go(turn string, blocks int) bool {
 	log.Debugf("Turning %s and traveling %d blocks", turn, blocks)
 
 	// Mod 4
@@ -27,49 +37,75 @@ func (l *Location) Go(turn string, blocks int) {
 		l.Orientation--
 	}
 
+	var axis *int
+	var modifier int
+
+	// Figure out which coordinate to change.
 	switch math.Mod(float64(l.Orientation), 4) {
 	case 0: // North
-		l.Y += blocks
-	case 1:
-		l.X += blocks
-	case 2:
-		l.Y -= blocks
-	case 3:
-		l.X -= blocks
+		axis = &l.Y
+		modifier = 1
+	case 1: // East
+		axis = &l.X
+		modifier = 1
+	case 2: // South
+		axis = &l.Y
+		modifier = -1
+	case 3: // West
+		axis = &l.X
+		modifier = -1
 	}
 
-	log.Debugf("You are at coordinates: %d,%d", l.X, l.Y)
-	log.Debugf("You are presently %d blocks from start", l.DistanceFromStart())
+	// Move one step in each dirction
+	for i := 1; i <= blocks; i++ {
+		*axis += 1 * modifier
+
+		log.Debugf("You are at coordinates: %s", l.Coords)
+		log.Debugf("You are presently %d blocks from start", l.DistanceFromStart())
+
+		if visited[*l.Coords] {
+			log.Debugf("Already visited %s, returning.", l.Coords)
+			return true
+		}
+		visited[*l.Coords] = true
+	}
+
+	return false
 }
 
 func (l *Location) DistanceFromStart() int {
 	return int((math.Abs(float64(l.X)) + math.Abs(float64(l.Y))))
 }
 
+func init() {
+	visited = make(map[Coords]bool)
+}
+
 func main() {
 	log.SetLevel(log.InfoLevel)
 
 	l := &Location{
-		Orientation: 0,
-		X:           0,
-		Y:           0,
+		Coords: &Coords{},
 	}
 
 	input, err := ioutil.ReadFile("input")
 	if err != nil {
 		log.Fatal(err)
 	}
-	s := strings.Trim(string(input), "\n")
 
-	directions := strings.Split(s, ", ")
-	for _, d := range directions {
+	s := strings.Trim(string(input), "\n")
+	for _, d := range strings.Split(s, ", ") {
 		direction := string(d[0])
 		distance, err := strconv.Atoi(d[1:])
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		l.Go(direction, distance)
+		if v := l.Go(direction, distance); v == true {
+			fmt.Printf("Visited %s twice\n", l.Coords)
+			fmt.Printf("It's %d blocks away\n", l.DistanceFromStart())
+			os.Exit(0)
+		}
 	}
 
 	fmt.Printf("You are: %d blocks away\n", l.DistanceFromStart())
