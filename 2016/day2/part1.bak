6e47d4a75f8b7b3630ecb253f9a09fb006e1bd95
Aaron Mills <fluxrad@gmail.com>
2016-12-06 08:17:19 -0700

    not working yet - part 2

---
+++ 2016/day2/part1.bak
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+func move(old, new int) int {
+	if (0 <= new) && (new <= 2) {
+		return new
+	}
+	return old
+}
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetLevel(log.InfoLevel)
+}
+
+func main() {
+	input, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	code := []int{}
+
+	// 3x3
+	var keypad = [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	// instruction map. note x and y coords are reversed
+	var imap = map[string][2]int{
+		// Y operations
+		"U": {-1, 0},
+		"D": {1, 0},
+		// X operations
+		"L": {0, -1},
+		"R": {0, 1},
+	}
+
+	// starting point
+	x := 1
+	y := 1
+
+	scanner := bufio.NewScanner(input)
+
+	// Get each line
+	for scanner.Scan() {
+		instructions := strings.Split(scanner.Text(), "")
+		for _, i := range instructions {
+			newY := move(y, y+imap[i][0])
+			newX := move(x, x+imap[i][1])
+
+			log.WithFields(log.Fields{
+				"number":      keypad[y][x],
+				"instruction": i,
+				"old coords":  fmt.Sprintf("[%d,%d]", x, y),
+				"new coords":  fmt.Sprintf("[%d,%d]", newX, newY),
+				"new number":  keypad[newY][newX],
+			}).Debug("Parsing instruction")
+
+			x = newX
+			y = newY
+
+		}
+		code = append(code, keypad[y][x])
+	}
+
+	fmt.Println("Key Code is:", code)
+}
---
+++ 2016/day2/part2.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+// 3x3
+var keypad = [][]string{
+	{"", "", "", "", "", "", ""},
+	{"", "", "", "1", "", "", ""},
+	{"", "", "2", "3", "4", "", ""},
+	{"", "5", "6", "7", "8", "9", ""},
+	{"", "", "A", "B", "C", "", ""},
+	{"", "", "", "D", "", "", ""},
+	{"", "", "", "", "", "", ""},
+}
+
+// instruction map. note x and y coords are reversed
+var imap = map[string][2]int{
+	// Y operations
+	"U": {-1, 0},
+	"D": {1, 0},
+	// X operations
+	"L": {0, -1},
+	"R": {0, 1},
+}
+
+func move(old, new []int) []int {
+	if keypad[new[0],new[1]] == "" {
+		return old
+	}
+	return new
+}
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetLevel(log.InfoLevel)
+}
+
+func main() {
+	input, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	code := []string{}
+
+	// starting point
+	yx := []int{3, 1}
+
+	scanner := bufio.NewScanner(input)
+
+	// Get each line
+	for scanner.Scan() {
+		instructions := strings.Split(scanner.Text(), "")
+		for _, i := range instructions {
+			newYX := [][]int{yx[0] + imap[i][0], yx[1] + imap[i][1]}
+
+			log.WithFields(log.Fields{
+				"number":      keypad[yx],
+				"instruction": i,
+				"old coords":  yx,
+				"new coords":  newYX,
+				"new number":  keypad[newYX],
+			}).Debug("Parsing instruction")
+
+			yx = move(yx, newYX)
+		}
+		code = append(code, keypad[y][x])
+	}
+
+	fmt.Println("Key Code is:", code)
+}
