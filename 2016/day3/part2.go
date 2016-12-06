cd2092eef7c607a9830aa26191ab60966598cfcf
Aaron Mills <aaron.mills@returnpath.com>
2016-12-06 12:05:48 -0700

    complete part 2

--- 2016/day3/part2.go
+++ 2016/day3/part2.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
-	"strings"
+	"regexp"
+	"sort"
+	"strconv"
 
 	log "github.com/Sirupsen/logrus"
 )
@@ -19,17 +22,43 @@ func main() {
 		log.Fatal(err)
 	}
 
-	possible := 0
+	// Input is a little janky so regex it out.
+	r := regexp.MustCompile("[0-9]+")
+
+	possibleTriangles := 0
 
 	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		possible += 1
-		tr := strings.Split(scanner.Text(), " ")
 
-		log.WithFields(log.Fields{
-			"sides":    tr,
-			"possible": possible,
-		}).Debug()
+	trs := [][]int{}
+	for scanner.Scan() {
+
+		for i, v := range r.FindAllString(scanner.Text(), -1) {
+			s, err := strconv.Atoi(v)
+			if err != nil {
+				log.Fatal("wtf")
+			}
+
+			// New batch of triangles
+			if len(trs) < 3 {
+				trs = append(trs, []int{s})
+			} else {
+				trs[i] = append(trs[i], s)
+			}
+		}
+
+		// Are all three triangles fully populated?
+		if len(trs) == 3 && len(trs[2]) == 3 {
+			for _, triangle := range trs {
+				sort.Ints(triangle)
+				if triangle[0]+triangle[1] > triangle[2] {
+					possibleTriangles++
+				}
+			}
+
+			// Start over
+			trs = trs[:0]
+		}
 	}
 
+	fmt.Printf("There are %d possible triangles\n", possibleTriangles)
 }
