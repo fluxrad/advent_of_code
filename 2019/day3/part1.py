6321af376c5843338500f5c82f296deb2110f537
Aaron Mills <aaron.mills@validity.com>
2019-12-03 13:49:19 -0700

    broke but let's commit it

--- 2019/day3/part1.py
+++ 2019/day3/part1.py
@@ -1,20 +1,85 @@
 #!/usr/bin/env python3
 
+import numpy as np
 
-def place_wire(grid, wire):
-    index = grid[0][0]
+def place_wire1(grid, wire):
+    posX, posY = 0, 0
+
     for i in wire:
         direction = i[0]
         distance = int(i[1:])
 
-## Main
+        if direction == 'R':
+            for _ in range(distance):
+                posX += 1
+                grid[posX, posY] = 1
+        elif direction == 'L':
+            for _ in range(distance):
+                posX -= 1
+                grid[posX, posY] = 1
+        elif direction == 'U':
+            for _ in range(distance):
+                posY += 1
+                grid[posX, posY] = 1
+        elif direction == 'D':
+            for _ in range(distance):
+                posY -= 1
+                grid[posX, posY] = 1
+
+    return grid
+
+
+def place_wire2(grid, wire, cross_coords):
+    posX, posY = 0, 0
+
+    for i in wire:
+        direction = i[0]
+        distance = int(i[1:])
+
+        if direction == 'R':
+            for _ in range(distance):
+                posX += 1
+                if grid[posX, posY] == 1:
+                    cross_coords.append([posX, posY])
+        elif direction == 'L':
+            for _ in range(distance):
+                posX -= 1
+                if grid[posX, posY] == 1:
+                    cross_coords.append([posX, posY])
+        elif direction == 'U':
+            for _ in range(distance):
+                posY += 1
+                if grid[posX, posY] == 1:
+                    cross_coords.append([posX, posY])
+        elif direction == 'D':
+            for _ in range(distance):
+                posY -= 1
+                if grid[posX, posY] == 1:
+                    cross_coords.append([posX, posY])
+
+    return grid, cross_coords
+# Main
+
+
 with open('input', 'r') as f:
     wires = f.read().splitlines()
 
 wire1 = wires[0].split(',')
 wire2 = wires[1].split(',')
 
-# TODO: figure out how to know what the appropriate size grid is based on the wires.
-grid = [[0] * 10000 for i in range(10000)]
+# TODO: figure out how to know what the appropriate size grid is based on the
+# wires.
+grid = np.zeros((100000, 100000), int)
 
-grid = place_wire(grid, wire1)
+cross_coords = []
+grid = place_wire1(grid, wire1)
+grid, cross_coords = place_wire2(grid, wire2, cross_coords)
+
+print(cross_coords)
+
+min_distance = abs(cross_coords[0][0]) + abs(cross_coords[0][1])
+for c in cross_coords:
+    distance = abs(c[0]) + abs(c[1])
+    min_distance = distance if distance < distance else min_distance
+
+print(f'The answer is {min_distance}')
