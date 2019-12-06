295fef7bd0962c6bbf50b7609205d60cdaed5071
Aaron Mills <fluxrad@gmail.com>
2019-12-06 07:21:34 -0700

    day6

---
+++ 2019/day6/part1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+class AdventOject:
+    def __init__(name):
+        self.orbits = 0
+        self.name = name
+
+    def set_orbit(obj):
+        self.orbits = obj
+
+    def orbit_count:
+        if self.orbits == 0
+        return orbit 
+
+with open('input', 'r') as f:
+    orbits  = f.read().splitlines()
