248bbca7808069a7bf37a4379c6807dd9c11176d
Aaron Mills <aaron.mills@validity.com>
2019-12-05 20:22:33 -0700

    update

--- 2019/day5/part2.py
+++ 2019/day5/part2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-DEBUG = True
+DEBUG = False
 
 def debug(s):
     if DEBUG:
@@ -72,9 +72,8 @@ def equals(memory, loc, mode1, mode2):
     return loc + 4
 
 
-# with open('input', 'r') as f:
-#     memory = list(map(int, f.read().split(',')))
-memory = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+with open('input', 'r') as f:
+    memory = list(map(int, f.read().split(',')))
 system_id = int(input("Please enter the system ID: "))
 
 halt = False
